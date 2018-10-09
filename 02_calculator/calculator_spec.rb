9ea0825d5cdf2f50b774c547cc3cd69ea1ab750a
kitsuner <lionel_renard@hotmail.com>
2018-10-09 11:06:20 +0200

    version corrigé

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -1,107 +1,33 @@
-# # Topics
-#
-# * functions
-# * math
-# * arrays
-# * iterating/looping
-#
-# # Calculator
-#
-# you will build a simple calculator script with the following methods:
-#
-# `add` takes two parameters and adds them
-#
-# `subtract` takes two parameters and subtracts the second from the first
-#
-# `sum` takes an *array* of parameters and adds them all together
-#
-# # Warning
-#
-# You may not have enough knowledge yet to complete `sum`. You will probably
-# need to use **loops** (e.g. `while`) or **iterators** (e.g. `each`) to
-# get the tests to pass.
-#
-# # Bonus
-#
-# There are also some bonus exercises for when you finish the regular ones. The bonus round will help teach you test-driven *development*, not simply test-guided *learning*.
-#
-# Your mission, should you choose to accept it, is to write *tests* for three new methods:
-#
-# * `multiply` which multiplies two numbers together
-# * `power` which raises one number to the power of another number
-# * `[factorial](http://en.wikipedia.org/wiki/Factorial)` (check Wikipedia if you forgot your high school math).
-#
-#
-
-require "calculator"
-
-describe "add" do
-  it "adds 0 and 0" do
-    expect(add(0,0)).to eq(0)
-  end
-
-  it "adds 2 and 2" do
-    expect(add(2,2)).to eq(4)
-  end
-
-  it "adds positive numbers" do
-    expect(add(2,6)).to eq(8)
-  end
+def add(a, b)
+  return (a+b)
 end
 
-describe "subtract" do
-  it "subtracts numbers" do
-    expect(subtract(10,4)).to eq(6)
-  end
+def subtract(a,b)
+  return (a - b)
 end
 
-describe "sum" do
-  it "computes the sum of an empty array" do
-    expect(sum([])).to eq(0)
-  end
-
-  it "computes the sum of an array of one number" do
-    expect(sum([7])).to eq(7)
-  end
-
-  it "computes the sum of an array of two numbers" do
-    expect(sum([7,11])).to eq(18)
-  end
-
-  it "computes the sum of an array of many numbers" do
-    expect(sum([1,3,5,7,9])).to eq(25)
-  end
+def sum(a)
+  b = 0
+  a.each {|value| b = b + value }
+  return b
 end
 
-# Let's write some tests !
-# we'll show you the first, you will write the others :)
-
-describe "#multiply" do
-
-  it "multiplies two numbers" do
-    expect(multiply(3, 4)).to eq(12)
-  end
-
-  it "multiplies two other numbers"
-    # one other test here, don't forget do end ;)
-    expect(multiply(6, 6)).to eq (36)
-  end
-
-  it "multiplies two other numbers, one of them being 0"
-    # one last test, with 0 in it
-  expect (multiply(0,0)).to eq (0)
+def multiply(a, b)
+  return (a * b)
 end
 
-describe "#power" do
-  it "raises one number to the power of another number"
-  
+def power(a, b)
+  c = 1
+  b.times { c = c * a }
+  return c
 end
 
-# http://en.wikipedia.org/wiki/Factorial
-describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+def factorial(a)
+  b = 1
+  c = 2
+  (a-1).times do
+    b = b * c
+    c = c + 1
+  end
+  return b
 end
